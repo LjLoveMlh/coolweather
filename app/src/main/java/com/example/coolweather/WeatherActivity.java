1253db7b0ae3a1113a74720314e07ceb394cb009
Tony <tony@gmail.com>
2017-09-25 22:44:30 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/example/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/coolweather/WeatherActivity.java
@@ -5,9 +5,13 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -27,6 +31,10 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class WeatherActivity extends AppCompatActivity {
+    public DrawerLayout drawerLayout;
+    private Button navButton;
+    public SwipeRefreshLayout swipeRefresh;
+    private String mWeatehrId;
     private ScrollView weatherLayout;
     private TextView titleCity;
     private TextView titleUpdateTime;
@@ -49,6 +57,8 @@ public class WeatherActivity extends AppCompatActivity {
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
         setContentView(R.layout.activity_weather);
+      drawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
+        navButton= (Button) findViewById(R.id.nav_button);
         bingPicImg= (ImageView) findViewById(R.id.bing_pic_img);
         weatherLayout= (ScrollView) findViewById(R.id.weather_layout);
         titleCity= (TextView) findViewById(R.id.title_city);
@@ -61,9 +71,17 @@ public class WeatherActivity extends AppCompatActivity {
         comfortText= (TextView) findViewById(R.id.comfort_text);
         carWashText= (TextView) findViewById(R.id.carwash_text);
         sportText= (TextView) findViewById(R.id.sport_text);
+        swipeRefresh= (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
         SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
         String weatherString=prefs.getString("weather",null);
         String bingPic=prefs.getString("bing_pic",null);
+        navButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
         if(bingPic!=null){
             Glide.with(this).load(bingPic).into(bingPicImg);
         }else{
@@ -71,12 +89,19 @@ public class WeatherActivity extends AppCompatActivity {
         }
         if(weatherString!=null){
             Weather weather= Utility.handleWeatherResponse(weatherString);
+            mWeatehrId=weather.basic.weatherId;
         showWeatherInfo(weather);
         }else{
-            String weatherId=getIntent().getStringExtra("weather_id");
+            mWeatehrId=getIntent().getStringExtra("weather_id");
             weatherLayout.setVisibility(View.INVISIBLE);
-            requestWeather(weatherId);
+            requestWeather(mWeatehrId);
         }
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                requestWeather(mWeatehrId);
+            }
+        });
     }
 
    public void requestWeather(final String weatherId) {
@@ -89,6 +114,7 @@ public class WeatherActivity extends AppCompatActivity {
                    @Override
                    public void run() {
                        Toast.makeText(WeatherActivity.this,"获取天气失败onfailure",Toast.LENGTH_SHORT).show();
+                  swipeRefresh.setRefreshing(false);
                    }
                });
            }
@@ -110,6 +136,7 @@ public class WeatherActivity extends AppCompatActivity {
                            Toast.makeText(WeatherActivity.this,"获取天气失败response",Toast.LENGTH_SHORT).show();
 
                        }
+                       swipeRefresh.setRefreshing(false);
                    }
                });
            }
